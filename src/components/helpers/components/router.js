de980352868306c06ba634ed5f33c2ef0dadec4b
alnrott <alanrotta99@hotmail.com>
2024-03-28 19:07:23 -0300

    Salved changes

--- src/components/helpers/components/router.js
+++ src/components/helpers/components/router.js
@@ -1,11 +1,5 @@
 import React, { useState, useEffect } from "react";
-import {
-  Navigate,
-  Route,
-  Routes,
-  useLocation,
-  useNavigate,
-} from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import Home from "../../home/home";
 import BouncingBallLoader from "./loader";
 import NextDates from "../../next-dates/next-dates";
